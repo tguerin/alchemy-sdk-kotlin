0c101b69ca0b57d529334dc3ee077834d8db048a
Tom <thomas.guerin2@gmail.com>
2022-09-18 19:49:11 -0500

    Add accounts method

--- alchemy-core/src/androidTest/kotlin/com/alchemy/sdk/core/CoreIntegrationTest.kt
+++ alchemy-core/src/androidTest/kotlin/com/alchemy/sdk/core/CoreIntegrationTest.kt
@@ -122,6 +122,12 @@ class CoreIntegrationTest {
         data.getOrThrow() shouldBeEqualTo  HexString.from("0x47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad")
     }
 
+    @Test
+    fun getAccounts() = runTest {
+        val data = alchemy.core.getAccounts()
+        data.getOrThrow() shouldBeEqualTo emptyList()
+    }
+
     private fun jsonReaderFromFileName(@IdRes fileRes: Int): JsonReader {
         return JsonReader(
             InputStreamReader(
--- alchemy-core/src/main/kotlin/com/alchemy/sdk/core/Core.kt
+++ alchemy-core/src/main/kotlin/com/alchemy/sdk/core/Core.kt
@@ -1,19 +1,10 @@
 package com.alchemy.sdk.core
 
 import com.alchemy.sdk.core.api.CoreApi
+import com.alchemy.sdk.core.model.Address
 
 class Core(private val coreApi: CoreApi) : CoreApi by coreApi {
 
-    /* fun getResolver(addressOrName: String): Flow<Result<String>> {
-        return jsonRpcClient.call(
-            url,
-            TransactionCallRequest(
-                Transaction(
-                    to = "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e",
-                    data = "0x0178b8bf" + Address.from(addressOrName).value.withoutPrefix()
-                )
-            ),
-            String::class.java
-        )
-    }*/
+    fun getAccounts(): Result<List<Address>> = Result.success(emptyList())
+
 }
