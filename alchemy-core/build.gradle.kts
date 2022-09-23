5db9398ec87fd75484ce1dd32afe37443ea631a9
Tom <thomas.guerin2@gmail.com>
2022-09-22 22:48:36 -0500

    Add simple release pipeline

--- alchemy-core/build.gradle.kts
+++ alchemy-core/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("kotlin")
+    id("java-library")
+    id("maven-publish")
 }
 
 dependencies {
@@ -15,4 +17,36 @@ dependencies {
     testImplementation(libs.test.kotlin.coroutines)
     testImplementation(libs.test.mockk)
     testImplementation(libs.test.mockk.agent)
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    from(sourceSets.main.get().allSource)
+    from({ project(":json-rpc-client").sourceSets.main.get().allSource })
+    archiveClassifier.set("sources")
+}
+
+tasks.jar {
+    from(project(":json-rpc-client").sourceSets.main.get().output.classesDirs)
+}
+
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            groupId = "com.github.tguerin"
+            artifactId = "alchemy-sdk-android"
+            version = "0.1.0"
+            artifact(sourcesJar.get())
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/tguerin/alchemy-sdk-android")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
