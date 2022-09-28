3b8006d04dcc60113db80d9d66fa850ffbcd205f
Tom <thomas.guerin2@gmail.com>
2022-09-28 17:51:51 -0500

    Formatting

--- alchemy-core/src/main/kotlin/com/alchemy/sdk/core/Transact.kt
+++ alchemy-core/src/main/kotlin/com/alchemy/sdk/core/Transact.kt
@@ -4,14 +4,20 @@ import com.alchemy.sdk.core.model.core.CancelPrivateTransactionRequest
 import com.alchemy.sdk.core.model.core.PrivateTransactionCall
 import com.alchemy.sdk.core.util.HexString
 
-class Transact(private val core: Core) {
+class Transact(
+    private val core: Core
+) {
 
-    suspend fun getTransaction(transactionHash: HexString) = core.getTransactionByHash(transactionHash)
+    suspend fun getTransaction(transactionHash: HexString) =
+        core.getTransactionByHash(transactionHash)
 
-    suspend fun sendTransaction(signedTransaction: HexString) = core.sendRawTransaction(signedTransaction)
+    suspend fun sendTransaction(signedTransaction: HexString) =
+        core.sendRawTransaction(signedTransaction)
 
-    suspend fun sendPrivateTransaction(privateTransactionCall: PrivateTransactionCall) = core.sendPrivateTransaction(privateTransactionCall)
+    suspend fun sendPrivateTransaction(privateTransactionCall: PrivateTransactionCall) =
+        core.sendPrivateTransaction(privateTransactionCall)
 
-    suspend fun sendPrivateTransaction(cancelPrivateTransactionRequest: CancelPrivateTransactionRequest) = core.cancelPrivateTransaction(cancelPrivateTransactionRequest)
+    suspend fun sendPrivateTransaction(cancelPrivateTransactionRequest: CancelPrivateTransactionRequest) =
+        core.cancelPrivateTransaction(cancelPrivateTransactionRequest)
 
 }
