1d6099fc6ea50885d45ca39543b952b5df44782f
Tom <thomas.guerin2@gmail.com>
2022-10-03 11:59:12 -0400

    Made fields of transaction call optional

--- alchemy-core/src/main/kotlin/com/alchemy/sdk/core/model/core/TransactionCall.kt
+++ alchemy-core/src/main/kotlin/com/alchemy/sdk/core/model/core/TransactionCall.kt
@@ -4,9 +4,10 @@ import com.alchemy.sdk.core.util.Ether
 import com.alchemy.sdk.core.util.HexString
 
 data class TransactionCall(
-    val from: Address?,
     val to: Address,
-    val gasPrice: Ether?,
-    val value: String?,
-    val data: HexString?
+    val from: Address? = null,
+    val gasPrice: Ether? = null,
+    val value: String? = null,
+    val data: HexString? = null,
+    val ccipReadEnabled: Boolean? = null,
 )
